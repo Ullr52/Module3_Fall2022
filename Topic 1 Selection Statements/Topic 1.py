98eb50ffaa06880398aa4818552923f0ee63c983
Ullr52 <97651235+Ullr52@users.noreply.github.com>
2022-09-11 23:07:32 -0500

    Topic one examples of if, else, else if (elif) statements

---
+++ Topic 1 Selection Statements/Topic 1.py
@@ -0,0 +1,16 @@
+#Sequential
+
+print('This is sequential')
+
+conditional = False
+#conditional
+if (conditional):
+        #statement below here
+    print('We are in the if branch') #if branch
+#else:
+    #print('We arein the else branch') #else branch
+elif (not conditional):
+    (print('now we are in the else if branch')) #else if combines else if statement inside of and else statement
+
+else:
+    print('this happens if nothing else')
